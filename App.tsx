58e67fa5a034633efa69355c26b3c20498c27c57
Akili Sosa <akilisosa@gmail.com>
2021-09-04 14:11:49 -0400

    connected fart and ass

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Image, Text, TouchableOpacity, View } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
-  const audio = new Audio("Recording.m4a");
 
-  const playSound = () => {
-      audio.play()
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound: sounds } = await Audio.Sound.createAsync(
+       require('./assets/Recording.m4a')
+    );
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sounds.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound?.unloadAsync(); }
+      : undefined;
+  }, [sound]);
+
+
   
-  }
-   return
-  
-  (
+  return (
     <View style={styles.container}>
       <Text>Fart Activated!</Text>
       <StatusBar style="auto" />
